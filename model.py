af21d5a3e28e1e471c985c900e42d47e94c17bfa
shishw <1485364623@qq.com>
2026-04-24 17:02:06 +0800

    Update model.py

--- model.py
+++ model.py
@@ -115,6 +115,7 @@ class CapsBAM(nn.Module):
         primary_capsules = self.spatial_attention(primary_capsules)
         return primary_capsules
 
+
 class DigitCaps(nn.Module):
     def __init__(self, num_capsules=2, num_routes=None, in_channels=8, out_channels=32):
         super(DigitCaps, self).__init__()
@@ -124,6 +125,7 @@ class DigitCaps(nn.Module):
         self.in_channels = in_channels
         self.num_routes = num_routes
         self.num_capsules = num_capsules
+        self.out_channels = out_channels
 
         self.W = nn.Parameter(torch.randn(1, num_routes, num_capsules, out_channels, in_channels))
 
@@ -133,21 +135,23 @@ class DigitCaps(nn.Module):
         W = torch.cat([self.W] * batch_size, dim=0)
         u_hat = torch.matmul(W, x)
         
-        b_ij = Variable(torch.zeros(1, self.num_routes, self.num_capsules, 1))
+        b_ij = Variable(torch.zeros(batch_size, self.num_routes, self.num_capsules, 1))
         if USE_CUDA:
             b_ij = b_ij.cuda()
 
         num_iterations = 3
         for iteration in range(num_iterations):
-            c_ij = F.softmax(b_ij)
-            c_ij = torch.cat([c_ij] * batch_size, dim=0).unsqueeze(4)
+            c_ij = F.softmax(b_ij, dim=1)
+            c_ij = c_ij.unsqueeze(4)
+            
             s_j = (c_ij * u_hat).sum(dim=1, keepdim=True)
             v_j = self.squash(s_j)
             
             if iteration < num_iterations - 1:
                 a_ij = torch.matmul(u_hat.transpose(3, 4), 
                                    torch.cat([v_j] * self.num_routes, dim=1))
-                b_ij = b_ij + a_ij.squeeze(4).mean(dim=0, keepdim=True)
+            
+                b_ij = b_ij + a_ij.squeeze(4)
 
         return v_j.squeeze(1)
 
@@ -155,7 +159,6 @@ class DigitCaps(nn.Module):
         squared_norm = (input_tensor ** 2).sum(-1, keepdim=True)
         output_tensor = squared_norm * input_tensor / ((1. + squared_norm) * torch.sqrt(squared_norm))
         return output_tensor
-
 
 class CapsNet(nn.Module):  
     def __init__(self, num_classes=2, Primary_capsule_num=4, input_size=(72, 72)):
